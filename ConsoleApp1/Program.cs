e92549c1de30587c25315281d3488350df0f0bca
labsfiap <labsfiap@PA106MICRO16>
2025-08-04 11:47:57 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System; // bibilioteca do sistema
+
+namespace MyApp // Namespace é um agrupamento de classes, métodos e outros namespaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue; // Define a cor de fundo do console
+            Console.ForegroundColor = ConsoleColor.White; // Define a cor do texto do console
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("teste teste teste");
+            Console.Write("teste teste teste");
+            Console.CursorSize = 50; 
+        }
+    }
+}
